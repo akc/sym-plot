490860c9a2e1e1f7d4a5382e4babe1e540670cdc
Anders Claesson <akc@akc.is>
2013-09-25 02:27:19 +0100

    Update to work with sym 0.9

--- Math/Sym/Plot.hs
+++ Math/Sym/Plot.hs
@@ -1,33 +1,32 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
 -- |
--- Module      : Math.Sym.Plot
--- Copyright   : (c) Anders Claesson 2013
--- License     : BSD-style
+-- Copyright   : Anders Claesson 2013
 -- Maintainer  : Anders Claesson <anders.claesson@gmail.com>
--- 
+--
 -- Plot permutations.
 
 module Math.Sym.Plot
     (
-     plotPerm, plotPerms, savePNG
+     Plot, plotPerm, plotPerms, savePlot
     ) where
 
 import Diagrams.Prelude
-import Graphics.Rendering.Diagrams.Core
-import Diagrams.Backend.Cairo.Internal
-import Math.Sym (Perm (..), st, toList, size)
+import Diagrams.Backend.Cairo
+import Data.Perm (toList)
+import Math.Sym
 
-type DC = Diagram Cairo R2
+-- | A plot is represented by Cairo diagram
+type Plot = Diagram Cairo R2
 
 -- An n x n grid
-grid :: Int -> DC
+grid :: Int -> Plot
 grid n = stripes `atop` rotateBy (1/4) stripes
     where 
       stripes = centerXY . hcat' with {sep = 1} . replicate n . vrule $ fromIntegral n
 
--- | The plot of a permutation as a Cairo diagram.
-plotPerm :: Perm a => a -> DC
+-- | The plot of a permutation.
+plotPerm :: Permutation a => a -> Plot
 plotPerm w = centerXY plot `atop` grid n `atop` background
     where
       n = size w
@@ -41,11 +40,12 @@ chunk :: Int -> [a] -> [[a]]
 chunk k [] = []
 chunk k xs = ys : chunk k zs where (ys, zs) = splitAt k xs
 
--- | The plot of a list of permutations as a Cairo diagram. The first
--- argument specifies how many permutations are on each row.
-plotPerms :: Perm a => Int -> [a] -> DC
+-- | The plot of a list of permutations. The first argument specifies
+-- how many permutations are on each row.
+plotPerms :: Permutation a => Int -> [a] -> Plot
 plotPerms cols = vcat . map hcat . chunk cols . map (pad 1.1 . plotPerm)
 
--- | Save a Cairo diagram as a PNG.
-savePNG :: Double -> String -> DC -> IO ()
-savePNG width filePath = fst . renderDia Cairo (CairoOptions filePath (Width width) PNG)
+-- | Save plot as a png, ps, pdf, or svg (determined automatically from
+-- the file extension).
+savePlot :: Double -> String -> Plot -> IO ()
+savePlot width filePath = renderCairo filePath (Width width)
